66489f37c710ddf5fb5839a2fcdd4059ff98d5c6
miotholerus <mio.tholerus@stud.sti.se>
2021-04-08 11:02:18 +0200

    8 april //Mio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ProgramList from './components/ProgramList';
 
 function App() {
   return (
     <div className="App">
+      <ProgramList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/ProgramList.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+
+const ProgramList = () => {
+    return (<div>
+        <h1 class="channel-title">
+            SVT 1
+        </h1>
+        <ul class="program-list">
+            <li class="program-list__item">
+                13:00<br/>
+                Vinterstudion
+            </li>
+            <li class="program-list__item">
+                13:30<br/>
+                Alpint: VM
+            </li>
+            <li class="program-list__item">
+                14:30<br/>
+                Vinterstudion
+            </li>
+        </ul>
+    </div>)
+}
+
+export default ProgramList;
